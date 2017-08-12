e5867c4ab80bfc5e8de1f2135e751dc78447198d
Mayank Shah <mayankshah7296@gmail.com>
2017-08-13 00:37:45 +0530

    Created 3 pages

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/resume',function(req, res){
+  res.send("Resume Will start downloading in short time!");
+});
+
+app.get('/profile-page',function(req, res){
+  res.send("Mayank's Profile is Great Please Hire him!!");
+});
+
+app.get('/portfolio',function(req, res){
+  res.send("Hi! Mayank's Portfolio has many projects Please Have a look in Resume");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
@@ -21,7 +33,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
+var port = 8000;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
